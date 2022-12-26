955cb3855be12c80ef53517297d4079dbf8f3339
Francisco Zuidwijk <electrocisko@gmail.com>
2022-12-26 16:52:23 -0300

    DTO

--- src/dto/ordersDTO.js
+++ src/dto/ordersDTO.js
@@ -2,7 +2,7 @@ const orderDtoPresenter = (orders) => {
   const ordersDTO = orders.map((el) => {
     return {
       orderNro: el.orderNro,
-      created_at: el.created_at.toDateString(),
+      created_at: el.created_at.toLocaleDateString(),
       buyerEmail: el.buyerEmail,
       status: el.status,
       items: el.items.length,
